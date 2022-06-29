5c15cf8394edbcc8abdf8effc3b346cf82abcd24
Rohit Tyagi <rtyagi@anar.biz>
2022-06-29 14:45:15 +0530

    react native 69 fix.

--- react-native.config.js
+++ react-native.config.js
@@ -1,13 +1,7 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   dependency: {
-    platforms: {
-      ios: { podspecPath: path.join(__dirname, 'react-native-appsflyer.podspec') },
-      android: {
-        packageImportPath: 'import com.appsflyer.reactnative.RNAppsFlyerPackage;',
-        packageInstance: 'new RNAppsFlyerPackage()',
-      },
-    },
+    platforms: {},
   },
 };
